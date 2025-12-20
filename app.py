d923a89751ebd9bffe6c7f954d687003d5792107
Salman Muhammad <99940448+Anything-Random@users.noreply.github.com>
2025-12-20 00:59:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,17 @@ app = Flask(__name__)
 @app.route("/attack", methods=["POST"])
 def attack_route():
     data = request.json
-    result = attack(data["attackers"], data["defenders"])
-    return jsonify({"multiplier": result})
+    return jsonify({
+        "multiplier": attack(data["attackers"], data["defenders"])
+    })
 
 @app.route("/coverage", methods=["POST"])
 def coverage_route():
-    data = request.json
-    result = coverage(data["moves"])
-    return jsonify(result)
+    return jsonify(coverage(request.json["moves"]))
 
 @app.route("/defence", methods=["POST"])
 def defence_route():
-    data = request.json
-    result = defence(data["types"])
-    return jsonify(result)
+    return jsonify(defence(request.json["types"]))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
