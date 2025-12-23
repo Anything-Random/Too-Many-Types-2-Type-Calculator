abf989c0e61589e9225857ee6931fbfd6e8c03c6
Anything-Random <sal1235@outlook.com>
2025-12-23 00:43:42 -0400

    Add Spreadsheet_Config.py

---
+++ Spreadsheet_Config.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+sheet = pd.read_csv("2 Many Types Spreadsheet - Type Chart.csv")
+types = sheet['Unnamed: 0'].tolist()
+for t in range(len(types)):
+    types[t] = types[t].title().strip()
+sheet.index = types
+sheet.drop('Unnamed: 0', axis=1, inplace=True)
+sheet.columns = types
+sheet.to_pickle('Type_Chart_2.pickle')
+
+types = sheet['Unnamed: 1'].to_dict()
+sheet.drop(['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 65'], axis=1, inplace=True)
+for i in types.keys():
+    types[i] = types[i].title()
+types[59] = 'OU'
+sheet.index = types.values()
+sheet.columns = types.values()
+for i in sheet.columns:
+    sheet[i] = pd.to_numeric(sheet[i])
+sheet.to_pickle('Type_Chart.pickle')
