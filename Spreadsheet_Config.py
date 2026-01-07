44d460331f9757bbe6abd9794f5eada502f6a2de
Anything-Random <sal1235@outlook.com>
2026-01-07 11:17:30 -0400

    Corrected Ludicolo and Exploud's typing in the spreadsheet and changed Vibes to Vibe to match the name in-game

--- Spreadsheet_Config.py
+++ Spreadsheet_Config.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+'''def configure_types():
+    sheet = pd.read_csv("Too Many Types Spreadsheet - Type Chart.csv")
+    types = sheet['Unnamed: 1'].to_dict()
+    sheet.drop(['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 65'], axis=1, inplace=True)
+    for i in types.keys():
+        types[i] = types[i].title()
+    types[59] = 'OU'
+    sheet.index = types.values()
+    sheet.columns = types.values()
+    for i in sheet.columns:
+        sheet[i] = pd.to_numeric(sheet[i])
+    sheet.to_pickle('Type_Chart.pickle')'''
+
 def configure_types_2():
     sheet = pd.read_csv("2 Many Types Spreadsheet - Type Chart.csv")
     types = sheet['Unnamed: 0'].tolist()
@@ -19,17 +32,6 @@ def configure_purity_dex():
     dex = pd.read_csv("Purity.csv", index_col='Name')
     dex.to_pickle('Purity_Dex.pickle')
 
-def configure_types():
-    sheet = pd.read_csv("Too Many Types Spreadsheet - Type Chart.csv")
-    types = sheet['Unnamed: 1'].to_dict()
-    sheet.drop(['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 65'], axis=1, inplace=True)
-    for i in types.keys():
-        types[i] = types[i].title()
-    types[59] = 'OU'
-    sheet.index = types.values()
-    sheet.columns = types.values()
-    for i in sheet.columns:
-        sheet[i] = pd.to_numeric(sheet[i])
-    sheet.to_pickle('Type_Chart.pickle')
-
+configure_types_2()
 configure_dex()
+configure_purity_dex()
